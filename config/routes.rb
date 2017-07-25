a688d6530290b3e4bc0e60dc8fa962cf81e9665d
gustavobonfim <bonfimoliveira@gmail.com>
2017-07-25 16:37:23 -0300

    add new create recipe with TDD

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
 
 
-root 'pages#home'
-get '/about', to: 'pages#about'
+  root 'pages#home'
+  get '/about', to: 'pages#about'
 
-get '/recipes', to: 'recipes#index'
+  get '/order', to: 'pages#order'
+
+  # get '/recipes', to: 'recipes#index'
+  # get 'recipes/new', to: 'recipes#new', as: 'new_recipe'
+  # post '/recipes', to: 'recipes#create'
+  # get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+
+
+  resources :recipes
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class RecipesTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = Chef.create!(chefname: "John", email: "john@example.com")
-    @recipe1 = Recipe.create!(name: "Italian Food", description: "pasta cooker for 20 min in the oven", chef: @user)
-    @recipe2 = Recipe.create!(name: "Brazilian Food", description: "bread cooker for 20 min in the oven", chef: @user)
+    @chef = Chef.create!(chefname: "John", email: "john@example.com")
+    @recipe1 = Recipe.create!(name: "Italian Food", description: "pasta cooker for 20 min in the oven", chef: @chef)
+    @recipe2 = Recipe.create!(name: "Brazilian Food", description: "bread cooker for 20 min in the oven", chef: @chef)
   end
 
   test "should get recipes index" do
@@ -15,9 +15,45 @@ class RecipesTest < ActionDispatch::IntegrationTest
 
   test "should get recipes listing" do
     get recipes_url
+    # para entregar o template
     assert_template 'recipes/index'
+    # para buscar palavras dentro de uma página html
     assert_match @recipe1.name, response.body
+    # para buscar um link específico
+    assert_select "a[href=?]", recipe_path(@recipe1), text: @recipe1.name
     assert_match @recipe2.name, response.body
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
   end
 
+  test "should get recipes show" do
+    get recipe_path(@recipe1)
+    assert_template 'recipes/show'
+    assert_match @recipe1.name.capitalize, response.body
+    assert_match @recipe1.description, response.body
+    assert_match @chef.chefname, response.body
+  end
+
+  test "create new valid recipe" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = 'chicken saute'
+    description_of_recipe = 'chicken cooked in 20 min in the oven, serve the delicious meal!'
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: {recipe: {name: name_of_recipe, description: description_of_recipe}}
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+  end
+
+  test "reject invalid recipe submissions" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: {recipe: {name: "", description: ""}}
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
 end

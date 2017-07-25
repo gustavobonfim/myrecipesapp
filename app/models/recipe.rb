d311375a5b1f167c1f6712406a2b126215afa984
gustavobonfim <bonfimoliveira@gmail.com>
2017-07-25 00:10:05 -0300

    prepare for TDD

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
 
+  validates :name, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725024608) do
+ActiveRecord::Schema.define(version: 20170725030055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chefs", force: :cascade do |t|
-    t.string "name"
+    t.string "chefname"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
